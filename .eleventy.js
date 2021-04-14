bf8a66c2c0ea0e7bc6a7439ea54af315c31ae353
June Hanabi <junehanabi@gmail.com>
2021-04-14 17:51:23 -0400

    Changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,33 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [64, 128, 256, 512, 1024, 2048, null],
+    formats: ["svg", "avif", "webp", "jpeg"],
+    urlPath: "/img/",
+    outputDir: "./_site/img/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline"
+  });
+}
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "src/icons": "/" });
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   return {
     dir: {
